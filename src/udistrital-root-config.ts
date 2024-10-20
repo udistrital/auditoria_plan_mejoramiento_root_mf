c03ec01c9621f9ea2ec96d6ed0cc43f4944955c5
edwargl7Z <heiner.granados@zinobe.com>
2024-10-20 12:42:56 -0500

    feat: Ajustes para el despliegue

--- src/udistrital-root-config.ts
+++ src/udistrital-root-config.ts
@@ -4,6 +4,8 @@ import {
   constructRoutes,
   constructLayoutEngine,
 } from "single-spa-layout";
+import "./assets/layout.css";
+
 import microfrontendLayout from "./microfrontend-layout.html";
 
 let environment;
